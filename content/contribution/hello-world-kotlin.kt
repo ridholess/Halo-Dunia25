d9bf4b3a667df7db221dc18584f18c71f02b5006
AutoFtbot <autoftbot@gmail.com>
2025-10-14 07:32:21 +0700

    Add Hello World in Kotlin, Swift, TypeScript, Scala, and Perl

---
+++ content/contribution/hello-world-kotlin.kt
@@ -0,0 +1,6 @@
+// Hello World in Kotlin
+// Hacktoberfest 2025
+
+fun main() {
+    println("Hello World")
+}
---
+++ content/contribution/hello-world-scala.scala
@@ -0,0 +1,8 @@
+// Hello World in Scala
+// Hacktoberfest 2025
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
