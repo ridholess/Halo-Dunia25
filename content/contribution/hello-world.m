cd425331c4c414d77c81b47fd9f12beaf6cc1e8c
azrilpramudia <azrilpramudia01@gmail.com>
2025-10-21 10:25:51 +0700

    hello world in objective-C

---
+++ content/contribution/hello-world.m
@@ -0,0 +1,11 @@
+// Hello world in Objective C
+// Hacktoberfest 2025
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+       NSLog(@"Hello, World!");
+   }
+   return 0;
+}
